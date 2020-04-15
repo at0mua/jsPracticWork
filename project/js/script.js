19e285bfb1cae288175d16a60ee804d2a74614b8
at0mua <zatom89@gmail.com>
2020-04-15 19:48:24 +0300

    added first task

---
+++ project/js/script.js
@@ -0,0 +1,22 @@
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+};
+
+let a = prompt("Введите обязательную статью расходов в этом месяце");
+let b = prompt("Во сколько обойдется?”");
+let c = prompt("Введите обязательную статью расходов в этом месяце");
+let d = prompt("Во сколько обойдется?”");
+
+appData.expenses[a] = b;
+
+appData.expenses[c] = d;
+
+alert(appData.budget / 30);
--- project/script.js
+++
@@ -1,7 +0,0 @@
-"use strick";
-
-console.log(leftBorderWidth);
-
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
